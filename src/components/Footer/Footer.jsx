d3a8ad0f2d9a8aa84be410d07d85c7c055b69e89
FatemehSaadatmand <fatemehsand82@gmail.com>
2025-02-21 16:28:24 +0330

    misplacing class instead of classname fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,21 +8,21 @@ const Footer = () => {
         <p className="newsletter-title">Sign Up for the <strong>NEWSLETTER</strong></p>
         <div className="newsletter-input">
           <input className="email-input" type="email" placeholder="Enter Your Email" />
-          <button className="btn subscribe-email"><i class="fa fa-envelope"></i> Subscribe</button>
+          <button className="btn subscribe-email"><i className="fa fa-envelope"></i> Subscribe</button>
         </div>
         <ul className="social-icons">
 
 								<li>
-									<a href="#"><i class="social-icons-i fa fa-facebook"></i></a>
+									<a href="#"><i className="social-icons-i fa fa-facebook"></i></a>
 								</li>
 								<li>
-									<a href="#"><i class="social-icons-i fa fa-twitter"></i></a>
+									<a href="#"><i className="social-icons-i fa fa-twitter"></i></a>
 								</li>
 								<li>
-									<a href="#"><i class="social-icons-i fa fa-instagram"></i></a>
+									<a href="#"><i className="social-icons-i fa fa-instagram"></i></a>
 								</li>
 								<li>
-									<a href="#"><i class="social-icons-i fa fa-pinterest"></i></a>
+									<a href="#"><i className="social-icons-i fa fa-pinterest"></i></a>
 								</li>
         </ul>
       </div>
@@ -31,9 +31,9 @@ const Footer = () => {
           <h4>ABOUT US</h4>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, dolores eligendi iure quia quae quasi sunt, cum qui reprehenderit reiciendis explicabo </p>
           <ul >
-									<li><a className="about-us-a" href="#"><i class="about-us-i fa fa-map-marker"></i>1734 Stonecoal Road</a></li>
-									<li><a className="about-us-a" href="#"><i class="about-us-i fa fa-phone"></i>+021-95-51-84</a></li>
-									<li><a className="about-us-a" href="#"><i class="about-us-i fa fa-envelope-o"></i>email@email.com</a></li>
+									<li><a className="about-us-a" href="#"><i className="about-us-i fa fa-map-marker"></i>1734 Stonecoal Road</a></li>
+									<li><a className="about-us-a" href="#"><i className="about-us-i fa fa-phone"></i>+021-95-51-84</a></li>
+									<li><a className="about-us-a" href="#"><i className="about-us-i fa fa-envelope-o"></i>email@email.com</a></li>
 								</ul>
         </div>
         <div className="categories">
@@ -71,15 +71,15 @@ const Footer = () => {
       </div>
       <div className="bottom-footer">
 						<div>
-							<ul class="footer-payments">
-								<li><a href="#"><i class="fa fa-cc-visa payment-icons"></i></a></li>
-								<li><a href="#"><i class="fa fa-credit-card payment-icons"></i></a></li>
-								<li><a href="#"><i class="fa fa-cc-paypal payment-icons"></i></a></li>
-								<li><a href="#"><i class="fa fa-cc-mastercard payment-icons"></i></a></li>
-								<li><a href="#"><i class="fa fa-cc-discover payment-icons"></i></a></li>
-								<li><a href="#"><i class="fa fa-cc-amex payment-icons"></i></a></li>
+							<ul className="footer-payments">
+								<li><a href="#"><i className="fa fa-cc-visa payment-icons"></i></a></li>
+								<li><a href="#"><i className="fa fa-credit-card payment-icons"></i></a></li>
+								<li><a href="#"><i className="fa fa-cc-paypal payment-icons"></i></a></li>
+								<li><a href="#"><i className="fa fa-cc-mastercard payment-icons"></i></a></li>
+								<li><a href="#"><i className="fa fa-cc-discover payment-icons"></i></a></li>
+								<li><a href="#"><i className="fa fa-cc-amex payment-icons"></i></a></li>
 							</ul>
-							<span class="copyright">
+							<span className="copyright">
 								Copyright ©2025 All rights reserved
 							</span>
 						</div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -79,7 +79,7 @@ const Header = ({
   return (
     <header className="header">
       <div className="top-bar">
-        <p><i class="color-red fa fa-phone"></i> +021-95-51-84 |<i class="color-red fa fa-envelope-o"></i> email@email.com |<i class="color-red fa fa-map-marker"></i> 1734 Stonecoal Road</p>
+        <p><i className="color-red fa fa-phone"></i> +021-95-51-84 |<i className="color-red fa fa-envelope-o"></i> email@email.com |<i className="color-red fa fa-map-marker"></i> 1734 Stonecoal Road</p>
         <div>
           <span><i className="color-red fa fa-dollar"></i> USD</span> | <span><i className="color-red fa fa-user-o"></i>My Account</span>
         </div>
@@ -112,9 +112,9 @@ const Header = ({
           <button className="btn searchbar-btn">Search</button>
         </div>
         <div className="cart-wish">
-        <i class="fa fa-heart-o"></i>
+        <i className="fa fa-heart-o"></i>
           <button className="btn btn-transparent">Wishlist</button>
-          <i class="fa fa-shopping-cart"></i>
+          <i className="fa fa-shopping-cart"></i>
           <button className="btn btn-transparent">Cart <span className="added-to-cart">{totalItems}</span></button>
         </div>
       </div>
@@ -122,7 +122,7 @@ const Header = ({
         <span>Total: ${totalPrice}</span>
       </div>
 			<div className="navbar">
-      <ul class="main-nav">
+      <ul className="main-nav">
 						<li className="navbar-items"><a className="navbar-a active-text" href="#">Home</a></li>
 						<li className="navbar-items"><a className="navbar-a" href="#">Categories</a></li>
 						<li className="navbar-items"><a className="navbar-a" href="#">Hot Deals</a></li>
