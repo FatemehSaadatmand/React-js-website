ce18fde16db16757f3e686575052f385be55c3fc
FatemehSaadatmand <fatemehsand82@gmail.com>
2025-02-28 17:22:31 +0330

    fixed

--- src/components/TopSelling/requests.js
+++ src/components/TopSelling/requests.js
@@ -1,7 +1,7 @@
 import { PRODUCTS_TOP_RATED_URL } from "../../configs/urls";
 
 export function fetchTopSellingProducts() {
-    return fetch(`${PRODUCTS_TOP_RATED_URL}`)
+    return fetch(PRODUCTS_TOP_RATED_URL)
       .then((response) => {
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
--- src/views/ProductPage/requests.js
+++ src/views/ProductPage/requests.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD:src/ProductPage/requests.js
 import { PRODUCTS_URL } from "../configs/urls";
-
 export function fetchProductDetails (id) {
     return fetch(`${PRODUCTS_URL}/${id}`)
-=======
-export function fetchProductDetails (id) {
-    return fetch(`https://kaaryar-ecom.liara.run/v1/products/${id}`)
->>>>>>> 8bed1fa72f2808a68b8ec67ae41b8cc9f748d5dc:src/views/ProductPage/requests.js
     .then((response) => {
       if (!response.ok) {
         throw new Error("Failed to fetch product details");
