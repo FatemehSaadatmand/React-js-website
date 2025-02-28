ed4c46be3d60823603ed0658c81f165566470483
FatemehSaadatmand <155776779+FatemehSaadatmand@users.noreply.github.com>
2025-02-28 15:42:08 +0330

    feat(Card): اضافه شدن قابلیت افزودن به لیست خرید
    * add to cart changes
    
    * add to cart changes
    
    * css changes removed
    
    * total price issue and cartItems update issue fixed
    
    * comments deleted
    
    * c.log removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import CategoryFilter from "./components/CategoryFilter/CategoryFilter";
@@ -10,14 +11,40 @@ import TopSelling from "./components/TopSelling/TopSelling";
 import ProductPage from "./ProductPage/ProductPage";
 
 const App = () => {
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [cartItems, setCartItems] = useState(
+    JSON.parse(localStorage.getItem("cartItems")) || []
+  );
+  const [selectedCategory, setSelectedCategory] = useState("All Categories");
   const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const handleCategoryChange = (category) => {
     setSelectedCategory(category);
     setCurrentPage(1);
   };
+
+  const handleAddToCart = (product, quantity) => {
+    const updatedCart = [...cartItems];
+    const productIndex = updatedCart.findIndex((item) => item.id === product.id);
+
+    if (productIndex === -1) {
+      updatedCart.push({ ...product, quantity });
+    } else {
+      updatedCart[productIndex].quantity += quantity;
+    }
+
+    setCartItems(updatedCart);
+  };
+
+  const totalPrice = cartItems
+    .reduce((total, item) => total + item.price * item.quantity, 0)
+    .toFixed(2);
+
+  const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
 
   return (
     <Router>
@@ -27,6 +54,10 @@ const App = () => {
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
         setCurrentPage={setCurrentPage}
+        cartItems={cartItems}
+        setCartItems={setCartItems}
+        totalPrice={totalPrice}
+        totalItems={totalItems}
       />
       <main>
         <Routes>
@@ -55,18 +86,16 @@ const App = () => {
                   </div>
                 </div>
               </div>
-            }
-          />
-          <Route path="/product/:id" element={<ProductPage />} />
-        </Routes>
-      </main>
+          }
+        />
+        <Route path="/product/:id" element={<ProductPage handleAddToCart={handleAddToCart} />} />
+      </Routes>
+     </main>
+
       <Footer />
     </Router>
   );
 };
 
 export default App;
-
-
-
 
